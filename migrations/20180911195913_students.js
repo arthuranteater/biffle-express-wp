b2bbb4cd26bb632be5c4ff7fd8a0e85bc59ecd2e
Hunt Applegate <huntapplegate@gmail.com>
2018-09-12 09:52:54 -0600

    added students

--- migrations/20180911195913_students.js
+++ migrations/20180911195913_students.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
+  return (
+    knex.schema.createTable("classmates"),
+    table => {
+      table.increments();
+      table.text("firstName");
+      table.text("lastName");
+      table.text("hometown");
+      table.text("prevOccupation");
+      table.integer("favoriteNum");
+      table.text("pastime");
+    }
+  );
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("classmates");
 };
--- seeds/01_students.js
+++ seeds/01_students.js
@@ -5,9 +5,39 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {firstName: 'Hunt', lastName: 'Applegate', hometown: 'Charleston', prevOccupation: 'Benefits Advisor', favoriteNum: 8, pastime: 'cooking'},
+        {firstName: 'Maddie', lastName: 'Kreamer', hometown: 'Des Moines, IA', prevOccupation: 'Server/Bartender', favoriteNum: 7, pastime: 'Backpacking'},
+        {firstName: 'Kris', lastName: 'Kamikawa', hometown: 'Mililani, HI', prevOccupation: 'Legislative Aide', favoriteNum: 11, pastime: 'easy climbing'},
+        {firstName:  'Jed' lastName:  'Prentup', hometown:  'Boulder', prevOccupation:  'PokerDealer', favoriteNum: 24, pastime: 'delving'},
+        {
+          firstName: 'Jose',
+          lastName: 'Stricklin',
+          hometown: 'San German',
+          prevOccupation: 'Retail',
+          faveNum: 4,
+          pastime:  'Shakespearean Elocution'
+       },
+       {firstName: 'Steve', lastName: 'Lopez', hometown: 'FrankFort', prevOccupation: 'Bounty Hunter', favoriteNum: 710, pastime: 'running'},
+       {
+        firstName: 'Conrad',
+        lastName: 'Wright',
+        homeTown: 'Durango',
+        preOccupation: 'Rural Development Consultant',
+        favNumber: 'infinity',
+        pastime: 'Bird Watching'
+        },
+        {firstName: 'Anhuelita', lastName: 'LaChica', hometown: ''Kazakhstan, prevOccupation: 'Masseuse', favoriteNum: 2, pastime: 'sleeping'},
+        {
+          firstName: 'Brice', 
+          lastName: 'Sinkule', 
+          hometown: 'Crete, NE', 
+          prevOccupation: 'Sale Consultant', 
+          favoriteNum: 12, 
+          pastime: 'Basketball'
+      },
+
       ]);
     });
 };
+
+{firstName: 'Hunt', lastName: 'Applegate', hometown: 'Charleston', prevOccupation: 'Benefits Advisor', favoriteNum: 8, pastime: 'cooking'}
