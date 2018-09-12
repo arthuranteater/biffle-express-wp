0989352fc3dfad3665c6f1237a3e3db98fd1e7ca
Hunt Applegate <huntapplegate@gmail.com>
2018-09-11 19:24:40 -0600

    made express

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express
-const app
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3003;
 
+app.listen(port, () => console.log(`listening on ${port}`));
 
+app.get("/", (request, response) => {
+  response.send("THE ROUTE WORKED");
+});
 
 // We will be writing our routes in this file
 // example: app.get()
 
-
-
-module.exports = app
+module.exports = app;
